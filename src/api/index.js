abb3b8e0402d8b5b055399a106e6927b4ffd07ba
Chudi <chudionubogu@yahoo.com>
2022-04-11 11:13:33 -0500

    quick save

--- src/api/index.js
+++ src/api/index.js
@@ -33,3 +33,19 @@ export const loginUser = async (username, password) => {
   const data = await response.json();
   return data;
 };
+
+export const getAllRoutines = async () => {
+  const response = await fetch(`${baseURL}/routines`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const getUserRoutines = async (username) => {
+  const response = await fetch(`${baseURL}/users/${username}/routines`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  return data;
+};
