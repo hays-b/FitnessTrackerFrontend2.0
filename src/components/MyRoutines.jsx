fdb02b358596a8c13484dce6e74cb3b4e9391a21
Hays <haysbondy@icloud.com>
2022-04-11 10:33:43 -0500

    My first commit

---
+++ src/components/MyRoutines.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MyRoutines = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyRoutines;
+
+// As a registered user on the My Routines tab, I want to:
+
+// be shown a form to create a new routine
+
+// the form should have text fields for name and goal
+// for each routine which is owned by me I should
+
+// be able to update the name and goal for the routine
+// be able to delete the entire routine
+// be able to add an activity to a routine via a small form which has a dropdown for all activities, an inputs for count and duration
+// be able to update the duration or count of any activity on the routine
+// be able to remove any activity from the routine
---
+++ src/components/Routines.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Routines = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Routines;
+
+// As any user on the Routines tab, I want to:
+
+// see a list of all public routines showing:
+// The routine name, goal, and creator's username
+// A list of activities for the routine, including their name, description, and duration and/or count
