eb5455d81b3860576b9803f33cd3be3e8932a130
Hays <haysbondy@icloud.com>
2022-04-11 20:07:01 -0500

    fixed getRoutines

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -16,14 +16,20 @@ const Routines = () => {
     <div id="routineList">
       {routines.map((routine, idx) => (
         <div key={routine.id}>
-          <h3>{routine.name}</h3>
-          <p>{routine.goal}</p>
-          <p>{routine.creatorName}</p>
-        </div>
-      ))}
-      {routines.map((activity, index) => (
-        <div key={activity.id}>
-          <p>{activity.activities}</p>
+          <h1>Routine: {routine.name}</h1>
+          <h4>Creator: {routine.creatorName}</h4>
+          <h2>Goal: {routine.goal}</h2>
+          <h3>Activities:</h3>
+          <div id="activityList">
+            {routine.activities.map((activity, idx) => (
+                <div key={activity.id}>
+                    <h4>Step {idx + 1}: {activity.name} </h4>
+                    <p>Description: {activity.description}</p>
+                    <p>Count: {activity.count}</p>
+                    <p>Duration: {activity.duration}</p>
+                </div>
+            ))}
+           </div> 
         </div>
       ))}
     </div>
